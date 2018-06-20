b7c9da11b49f56281821c09ba3503b8d3417261c
Erik Nuber <erik.nuber@yahoo.com>
2018-06-20 07:38:37 -0700

    working on finishing game

--- domManEvents/app.js
+++ domManEvents/app.js
@@ -40,6 +40,43 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
     //3: update the round score if the rolled number was not a 1
+    if(dice !== 1) {
+        //add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //next player
+        nextPlayer();
+    }
+
 
 
 }); //on addEventlistener you have the event first and the function to run, you can also run the function directly there
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    //add current score to global score
+    scores[activePlayer] += roundScore;
+    //update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    //check for win condition
+    if (scores[activePlayer] >= 100) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        //next player
+        nextPlayer();
+    }
+});
+
+function nextPlayer() {
+    //turnery operator ? acts as true or false if true does the first part if false does second part;
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display = 'none';
+}
