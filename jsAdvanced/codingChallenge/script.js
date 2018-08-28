f22673a149f1535884329601b1a201e2b68e80ec
Erik Nuber <erik.nuber@yahoo.com>
2018-08-28 08:30:23 -0700

    finsihed advanced coding challenge

--- jsAdvanced/codingChallenge/script.js
+++ jsAdvanced/codingChallenge/script.js
@@ -16,26 +16,58 @@
             console.log(i + ': ' + this.answers[i]);
         }
     };
-    Question.prototype.checkAnswer = function(answer) {
+    Question.prototype.checkAnswer = function(answer, callback) {
+        var sc;
         if (answer === this.correct) {
             console.log('Correct Answer!');
+            sc = callback(true);
         } else {
             console.log('Wrong Answer');
+            sc = callback(false);
         }
+        this.displayScore(sc);
     };
 
+    Question.prototype.displayScore = function(score) {
+        console.log('Your current score is: ' + score);
+        console.log('-------------------------------------------------------');
+    };
 
-    var q1 = new Question('Is JavaScript the coolest programming language in the world?', ['Yes', 'No'], 0);
-    var q2 = new Question('What\'s the name of this course\'s teacher?', ['John', 'Micheal', 'Jonas'], 2);
-    var q3 = new Question('What does best describe coding', ['Boring', 'Hard', 'Fun', 'Tedious'], 2);
+        var q1 = new Question('Is JavaScript the coolest programming language in the world?', ['Yes', 'No'], 0);
+        var q2 = new Question('What\'s the name of this course\'s teacher?', ['John', 'Micheal', 'Jonas'], 2);
+        var q3 = new Question('What does best describe coding', ['Boring', 'Hard', 'Fun', 'Tedious'], 2);
 
-    var questions = [q1, q2, q3];
+        var questions = [q1, q2, q3];
 
-    var n = Math.floor(Math.random() * questions.length);
+    //this is a closure, we are returning a function that has access to the sc variable of the outer scope so that it
+    //can increase the score. You can set multiple variables to this function and, each will be independent.
+        function score() {
+            var sc = 0;
+            return function(correct) {
+                if (correct) {
+                    sc++;
+                }
+                return sc;
+            }
+        }
 
-    questions[n].displayQuestion();
-//parseInt() converts a string to a number;
-    var answer = parseInt(prompt('Please select the correct answer'));
-    questions[n].checkAnswer(answer);
+        var keepScore = score();
+
+        function nextQuestion() {
+
+            var n = Math.floor(Math.random() * questions.length);
+
+            questions[n].displayQuestion();
+    //parseInt() converts a string to a number;
+            var answer = prompt('Please select the correct answer. Type exit to leave.');
+
+            //allows you to exit out if user puts in the word exit. Otherwise it
+            if (answer.toLowerCase() !== 'exit') {
+                questions[n].checkAnswer(parseInt(answer), keepScore);
+                nextQuestion();
+        }
+    }
+
+    nextQuestion();
 })();
 
